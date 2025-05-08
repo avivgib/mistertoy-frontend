538eba410a392d2943ae3b227885f7218a6208c7
Aviv Gibli <avivgib1012@gmail.com>
2025-05-08 05:43:42 +0300

    Enhance UserMsg component with animation effects for message display and closure

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,17 +1,17 @@
+import 'animate.css'
 import { useState, useEffect, useRef } from 'react'
-
 import { eventBusService } from "../services/event-bus.service.js"
 
 export function UserMsg() {
-
     const [msg, setMsg] = useState(null)
+    const [animation, setAnimation] = useState('animate__bounce')
     const timeoutIdRef = useRef()
 
     useEffect(() => {
         const unsubscribe = eventBusService.on('show-user-msg', (msg) => {
             setMsg(msg)
+            setAnimation('animate__fadeInDown')
             if (timeoutIdRef.current) {
-                timeoutIdRef.current = null
                 clearTimeout(timeoutIdRef.current)
             }
             timeoutIdRef.current = setTimeout(closeMsg, 3000)
@@ -20,12 +20,14 @@ export function UserMsg() {
     }, [])
 
     function closeMsg() {
-        setMsg(null)
+        setAnimation('animate__fadeOutUp')
+        setTimeout(() => setMsg(null), 1000)
     }
 
     if (!msg) return <span></span>
+
     return (
-        <section className={`user-msg ${msg.type}`}>
+        <section className={`user-msg ${msg.type} ${animation} animate__animated`}>
             <button onClick={closeMsg}>x</button>
             {msg.txt}
         </section>
