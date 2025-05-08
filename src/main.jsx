c1cec8b5dc6fad4123dde0ab7d80ca0f64fea0da
Aviv Gibli <avivgib1012@gmail.com>
2025-05-08 05:01:11 +0300

    Refactor index.html and main.jsx: update title and stylesheet references; remove unused scripts; switch to SCSS for styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './assets/style/main.css'
+import './assets/style/main.scss'
 import { App } from './root-cmp.jsx'
 
 createRoot(document.getElementById('root')).render(
