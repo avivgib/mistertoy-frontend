b2a776d677b5aeb4266d7fb7cc4d5564f9428783
Aviv Gibli <avivgib1012@gmail.com>
2025-05-20 00:58:06 +0300

    Refactor routing and authentication components for improved structure and clarity

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,39 +1,36 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router } from 'react-router'
-// const Router = ReactRouterDOM.HashRouter
-import { Route, Routes } from 'react-router-dom'
-import { Provider } from 'react-redux'
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { Provider, useSelector } from 'react-redux'
 
 import { AppHeader } from './cmps/AppHeader.jsx'
-// import { AppFooter } from './cmps/AppFooter.jsx'
-
 import { HomePage } from './pages/HomePage.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { ToyIndex } from './pages/ToyIndex.jsx'
-import { store } from './store/store.js'
 import { ToyEdit } from './pages/ToyEdit.jsx'
 import { ToyDetails } from './pages/ToyDetails.jsx'
 import { UserDetails } from './pages/UserDetails.jsx'
-import { LoginSignup } from './cmps/LoginSignup.jsx'
+import { LoginSignup } from './cmps/auth/LoginSignup.jsx'
+import { AdminIndex } from './pages/AdminIndex.jsx'
+import { Login } from './cmps/auth/Login.jsx'
+import { Signup } from './cmps/auth/Signup.jsx'
+
+import { store } from './store/store.js'
+import { userService } from './services/user.service.js'
 
 const themes = [
     "theme-light",
     "theme-dark",
     "theme-cheerful",
     "theme-pastel",
-    // "theme-neon",
     "theme-natural"
-  ]
+]
 
 export function App() {
     useEffect(() => {
         const savedTheme = localStorage.getItem("preferred-theme") || "theme-light"
         document.documentElement.classList.remove(...themes)
-        if (savedTheme !== "theme-light") {
-          document.documentElement.classList.add(savedTheme)
-        }
-      }, [])
+        document.documentElement.classList.add(savedTheme)
+    }, [])
 
     return (
         <Provider store={store}>
@@ -43,13 +40,20 @@ export function App() {
                     <main className='main-layout'>
                         <Routes>
                             <Route element={<HomePage />} path="/" />
-                            <Route element={<LoginSignup />} path="/login" />
                             <Route element={<AboutUs />} path="/about" />
                             <Route element={<ToyIndex />} path="/toy" />
-                            <Route element={<ToyEdit />} path="/toy/edit" />
-                            <Route element={<ToyEdit />} path="/toy/edit/:toyId" />
+                            <Route element={<ToyEdit />} path="/toy/edit/:toyId?" />
                             <Route element={<ToyDetails />} path="/toy/:toyId" />
                             <Route element={<UserDetails />} path="/user/:userId" />
+                            <Route path="admin" element={
+                                <AuthGuard checkAdmin={true}>
+                                    <AdminIndex />
+                                </AuthGuard>
+                            } />
+                            <Route element={<LoginSignup />} path="login" >
+                                <Route index element={<Login />} />
+                                <Route path="signup" element={<Signup />} />
+                            </Route>
                         </Routes>
                     </main>
                     {/* <AppFooter /> */}
@@ -57,4 +61,12 @@ export function App() {
             </Router>
         </Provider>
     )
+}
+
+function AuthGuard({ children, checkAdmin = false }) {
+    // const user = userService.getLoggedinUser()
+    const user = useSelector(storeState => storeState.userModule.loggedInUser)
+    const isNotAllowed = !user || (checkAdmin && !user.isAdmin)
+    if (isNotAllowed) return <Navigate to="/" />
+    return children
 }
